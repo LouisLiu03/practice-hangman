ba589ff5da5a34e42163d9073335e4177d9bf47f
LouisLiu03 <dalion00@gmail.com>
2022-12-12 20:40:13 +1100

    editApp

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
+import { useState } from 'react';
+import words from './wordList.json';
+
 function App() {
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)]
+  })
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
 
 
   return (
-    <h1> Hi </h1>
+    <div 
+      style ={{
+        maxWidth: '800px',
+        display: 'flex',
+        flex: 'column',
+        gap: '2rem',
+        margin: '0 auto',
+        alignItems: 'center'
+      }}>
+        <div style={{fontSize: '2rem', textAlign: 'center'}}>
+          Lose
+          Win
+        </div>
+
+    </div>
   )
 }
 
